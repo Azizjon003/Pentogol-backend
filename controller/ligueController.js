b05a9f94c3462a9bc1d09e1e510da55f62e3ee5d
Azizjon <aliqulovazizjon68@gmail.com>
2023-05-05 09:49:29 +0500

    Saqlandi

--- controller/ligueController.js
+++ controller/ligueController.js
@@ -16,54 +16,56 @@ exports.getOneLigueComands = catchAsync(async (req, res, next) => {
   const startTime = req.query.startTime || 2021;
   const endTime = req.query.endTime || 2022;
   console.log(startTime, endTime);
-  const data = await Liga.findOne(
-    {
-      where: {
-        id: req.params.id,
-      },
-      include: [
-        {
-          model: db.seasons,
-          as: "seasons",
-          include: [
-            {
-              model: db.teamsSeasons,
-              as: "jamolar",
-              include: [
-                {
-                  model: db.teams,
-                  as: "team",
-                  attributes: ["id", "name", "image"],
-                },
-              ],
-
-              attributes: ["id", "teamId", "points", "goalRatio", "currentTur"],
-              // orders: [["points", "DESC"]],
-            },
-          ],
-          where: {
-            [db.Op.and]: [
-              { startTime: { [db.Op.gte]: startTime } },
-              { endTime: { [db.Op.lte]: endTime } },
-            ],
-          },
-
-          attributes: ["id", "startTime", "endTime"],
-        },
-      ],
-      attributes: ["id", "name", "seasons.id"],
+  const data = await Liga.findOne({
+    where: {
+      id: req.params.id,
     },
-    {
-      orders: [
-        [
-          { model: db.seasons, as: "liga" },
-          { model: db.teamsSeasons, as: "jamolar" },
-          "points",
-          "DESC",
+    include: [
+      {
+        model: db.seasons,
+        as: "seasons",
+        include: [
+          {
+            model: db.teamsSeasons,
+            as: "jamolar",
+            include: [
+              {
+                model: db.teams,
+                as: "team",
+                attributes: ["id", "name", "image"],
+              },
+            ],
+
+            attributes: ["id", "teamId", "points", "goalRatio", "currentTur"],
+            // orders: [["points", "DESC"]],
+          },
         ],
+        where: {
+          [db.Op.and]: [
+            { startTime: { [db.Op.gte]: startTime } },
+            { endTime: { [db.Op.lte]: endTime } },
+          ],
+        },
+
+        attributes: ["id", "startTime", "endTime"],
+      },
+    ],
+    attributes: ["id", "name", "seasons.id"],
+    order: [
+      [
+        { model: db.seasons, as: "seasons" },
+        { model: db.teamsSeasons, as: "jamolar" },
+        "points",
+        "DESC",
       ],
-    }
-  );
+      [
+        { model: db.seasons, as: "seasons" },
+        { model: db.teamsSeasons, as: "jamolar" },
+        "goalRatio",
+        "DESC",
+      ],  
+    ],
+  });
 
   res.status(200).json({
     status: "OK",
